298926d63efc8b6fbc1428d8bda2d518811e2f42
Sakthi Namasivayam <60822530+Sakthi299@users.noreply.github.com>
2020-10-02 14:01:01 +0530

    Demo_app

---
+++ flutter_auth/lib/Screens/Dashboard/dashboard_home.dart
@@ -0,0 +1,13 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_auth/Screens/Dashboard/components/body.dart';
+
+class LoginScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Body(),
+    );
+  }
+}
